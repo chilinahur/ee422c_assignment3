fe53f4807c65bd3cbe5c395950765ea11dc69da0
chilinahur <schilukuri@utexas.edu>
2016-09-26 20:54:41 -0500

    fixed header

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -1,14 +1,14 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Sriram Chilukuri
+ * smc4474
+ * 16445
+ * Yousef Abdelrazzaq
+ * Yja87
+ * 16445
  * Slip days used: <0>
- * Git URL:
+ * Git URL:https://github.com/chilinahur/ee422c_assignment3
  * Fall 2016
  */
 
