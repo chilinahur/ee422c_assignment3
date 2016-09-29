17a13a8ce097dd4c203b4156c4e04f03ff7f3c86
yousefab95 <yousef.abdelrazzaq@utexas.edu>
2016-09-29 15:13:20 -0700

    bfs added

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -13,7 +13,7 @@
  */
 
 
-//package assignment3;
+package assignment3;
 import java.util.*;
 import java.io.*;
 
@@ -21,7 +21,16 @@ public class Main {
 	
 	// static variables and constants only here.
 	
-	//	static Stack<String> s = new Stack<String>();
+	static class Node{
+		String data;
+		Node parent;
+ 		Node(String value, Node mom){
+ 			data=value;
+ 			parent = mom;
+ 		}
+ 	}	
+	static String startGlobe;
+	static String endGlobe;
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -36,105 +45,17 @@ public class Main {
 			kb = new Scanner(System.in);// default from Stdin
 			ps = System.out;			// default to Stdout
 		}
+		
+		//our TEST code
 		initialize();
-		//parse(kb);
-		//String[] wordArray = getAllMutants("start");
-		/*for(int i = 0; i < wordArray.length; i++){
-			System.out.println(wordArray[i]);
-		}*/
-		/*
-		 * 		initialize();
 		ArrayList<String> inputVals = parse(kb);	//get input
-		getWordLadderDFS(inputVals.get(0), inputVals.get(1));	//output DFS
-		getWordLadderBFS(inputVals.get(0), inputVals.get(1));	//output BFS
-		 */
-		ArrayList<String> ladder = getWordLadderDFS("cones", "smart");
-		System.out.println(ladder.toString());
-		System.out.println(ladder.size());
+		startGlobe = inputVals.get(0);
+		endGlobe = inputVals.get(1);
+		//ArrayList<String> ladder = getWordLadderDFS(startGlobe, endGlobe);	//output DFS
+		ArrayList<String> ladder = getWordLadderBFS(startGlobe, endGlobe);	//output BFS
+		printLadder(ladder);
 		
 		// TODO methods to read in words, output ladder
-	}
-	private static String[] getAllMutants(String input, Set<String> dictionary){
-		String[] iterations = new String[input.length() * 25];
-		int count = 0;
-		for(int i = 0; i<input.length(); i++){
-			StringBuilder temp = new StringBuilder(input);
-			char startVal = input.charAt(i);
-			for(char x = 'a'; x<='z'; x++){
-				temp.setCharAt(i, x);
-			if(startVal != temp.charAt(i) && dictionary.contains(temp.toString().toUpperCase())){
-				iterations[count] = temp.toString();
-				count++;
-			}
-			}
-		}
-		return iterations;	
-	}
-	/*
-	public static String[] getAllMutants(String word){
-		
-			int length = word.length();
-			String[] wordArray = new String[130];
-			String[] originalArray = new String[130];
-			String[] charArray = new String[length];
-			
-			for(int i = 0; i < length; i++){
-				charArray[i] = Character.toString(word.charAt(i));
-			}
-			System.arraycopy(charArray, 0, originalArray, 0, length);
-			//return array;
-			int k = 0;
-			for(int i = 0; i < 5; i++){
-				for(int j = 0; j < 26; j++){		
-					char x = (char) ('a' + j);
-					charArray[i] = Character.toString(x);
-					wordArray[k] = (charArray[0] + charArray[1] + charArray[2] + charArray[3] + charArray[4]);
-					k++;
-				}
-				charArray[i] = originalArray[i];
-			}
-			
-			Set <String> dict = makeDictionary();
-			String[] finalArray = new String[50];
-			
-			int count = 0;
-			for(int i = 0; i < wordArray.length; i++){
-				if(dict.contains(wordArray[i].toUpperCase()) && !wordArray[i].equals(word)){
-					finalArray[count] = wordArray[i];
-					count++;
-				}
-			}
-			
-			return finalArray;
-		
-	}
-	*/
-	
-	public static ArrayList<String> recDFS(ArrayList<String> visited, String start, String end){
-		
-		ArrayList<String> ladder = new ArrayList<String>();
-		String[] mutants = getAllMutants(start);
-		visited.add(start);
-		
-		if(start.equals(end)){
-			ladder.add(end);
-			return ladder;
-		}
-		
-		ladder.add(start);
-		
-		for(int i = 0; i < mutants.length; i++){
-				if(!visited.contains(mutants[i]) && mutants[i] != null){
-					ladder = recDFS(visited, mutants[i], end);
-				}
-				if(ladder != null && ladder.get(0).equals(end)){
-					ladder.add(start);
-					return ladder;
-				}
-		}
-		
-		return null;
-		
 	}
 	
 	public static void initialize() {
@@ -149,82 +70,109 @@ public class Main {
 	 * If command is /quit, return empty ArrayList. 
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
-		
 		ArrayList<String> inputList = new ArrayList<String>();
-		String val = keyboard.nextLine();
-		if(val == "/quit"){
-			return inputList;
+		String val = keyboard.nextLine().toLowerCase();
+		
+		if(val.contains("/quit")){
+			System.exit(0);
 		}
-		inputList.add(0, val);
-		inputList.add(1, keyboard.nextLine());
-		//System.out.println(Arrays.toString(inputList.toArray()));
+		else{
+			String[] val2 = val.split("\\s+");
+			inputList.add(0, val2[0]);
+			inputList.add(1, val2[1]);
+		}
 		return inputList;
 	}
-	
-	/*
-	 * 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		// Returned list should be ordered start to end.  Include start and end.
-		// Return empty list if no ladder.
-		// TODO some code
-		Set<String> dict = makeDictionary();
-		s.push(start);
-		String[] starterVals = getAllMutants(start, dict);
-		for(int i = 0; i < starterVals.length; i++){		//takes into account all iterations
-			if(starterVals[i] != null && starterVals[i] != end && !s.contains(starterVals[i]) ){
-			getWordLadderDFS(starterVals[i], end);
-			}
-			else if(starterVals[i]==end){
-				break;
-			}
-			else if(starterVals[i]==null){
-				break;
-			}
-			else if(s.contains(starterVals[i])){
-				break;
-			}
-		}
-		if(!s.contains(end)){
-			s.pop();
-		}
-		// TODO more code
 		
-		return null; // replace this line later with real return
-	}
-	 */
-	
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
 		
 		// Returned list should be ordered start to end.  Include start and end.
 		// Return empty list if no ladder.
 		// TODO some code
-		
+				
 		Set<String> dict = makeDictionary();
 		
 		ArrayList<String> visited = new ArrayList<String>();
 		ArrayList<String> ladder = new ArrayList<String>();
-		
-		ladder = recDFS(visited, start, end);
+
+		ladder = recDFS(visited, start, end, dict);
 
 		
 		// TODO more code
 		
 		if(ladder == null){
-			System.out.println("No such ladder");
-			ladder.add("empty");
-			return ladder;
+			ArrayList<String> ladderError = new ArrayList<String>();
+			return ladderError;
 		}
 		
-		//System.out.println("WE'RE DONE");
-		return ladder; // replace this line later with real return
+		Collections.reverse(ladder);
+		return ladder;
 	}
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
-		
 		// TODO some code
 		Set<String> dict = makeDictionary();
 		// TODO more code
 		
-		return null; // replace this line later with real return
+		Queue<Node> queue = new LinkedList<Node>();
+		
+		ArrayList<String> visited = new ArrayList<String>();
+		ArrayList<String> ladder = new ArrayList<String>();
+		
+		Node parent = new Node(start, null);
+		
+		queue.add(parent);
+		
+		while(!queue.isEmpty()){
+			
+			Node pointer = queue.element();	//head of queue
+			
+			if(pointer.data == null){
+				ArrayList<String> ladderError = new ArrayList<String>();
+				return ladderError;
+			}
+			
+			String[] mutants = getAllMutants(pointer.data, dict);
+			
+			while(visited.contains(pointer.data)){
+				
+					if(queue.isEmpty()){
+						ArrayList<String> ladderError = new ArrayList<String>();
+						return ladderError;
+					}
+					
+					queue.remove();
+					pointer = queue.element();
+					mutants = getAllMutants(pointer.data, dict);
+			}
+			
+			int length = mutants.length;
+						
+			for(int i = 0; i < length; i++){
+				if(mutants[i] != null){
+					Node child = new Node(mutants[i], pointer);
+					if(!visited.contains(child.data)) queue.add(child);	
+				}
+			}
+			
+			if((queue.element().data).equals(end)){
+				Node child = queue.element();
+				Node mom = child.parent;
+				
+				while(mom != null){
+					ladder.add(child.data);
+					child = child.parent;
+					mom = mom.parent;
+				}
+				ladder.add(start);
+				Collections.reverse(ladder);
+				return ladder;
+			}
+			visited.add(queue.element().data);
+			queue.remove();
+		}
+		ArrayList<String> ladderError = new ArrayList<String>();
+		return ladderError;
 	}
     
 	public static Set<String>  makeDictionary () {
@@ -244,8 +192,59 @@ public class Main {
 	}
 	
 	public static void printLadder(ArrayList<String> ladder) {
-		
+		if(ladder.isEmpty()){
+			System.out.println("no word ladder can be found between " + startGlobe + " and " + endGlobe + ".");
+		}
+		else{
+		int size = ladder.size();
+		System.out.println("a "+ (size - 2) + "-rung word ladder exists between " + startGlobe + " and " + endGlobe + ".");
+		for(int i = 0; i<(size); i++){
+			System.out.println(ladder.get(i));
+		}
+		}
 	}
 	// TODO
 	// Other private static methods here
+	
+	private static String[] getAllMutants(String input, Set<String> dictionary){
+		String[] iterations = new String[input.length() * 25];
+		int count = 0;
+		for(int i = 0; i<input.length(); i++){
+			StringBuilder temp = new StringBuilder(input);
+			char startVal = input.charAt(i);
+			for(char x = 'a'; x<='z'; x++){
+				temp.setCharAt(i, x);
+			if(startVal != temp.charAt(i) && dictionary.contains(temp.toString().toUpperCase())){
+				iterations[count] = temp.toString();
+				count++;
+			}
+			}
+		}
+		return iterations;
+	}
+	
+	private static ArrayList<String> recDFS(ArrayList<String> visited, String start, String end, Set<String> dict){
+		
+		ArrayList<String> ladder = new ArrayList<String>();
+		String[] mutants = getAllMutants(start, dict);
+		visited.add(start);
+		
+		if(start.equals(end)){
+			ladder.add(end);
+			return ladder;
+		}
+		
+		ladder.add(start);
+		
+		for(int i = 0; i < mutants.length; i++){
+				if(!visited.contains(mutants[i]) && mutants[i] != null){
+					ladder = recDFS(visited, mutants[i], end, dict);
+				}
+				if(ladder != null && ladder.get(0).equals(end)){
+					ladder.add(start);
+					return ladder;
+				}
+		}
+		return null;	
+	}
 }
